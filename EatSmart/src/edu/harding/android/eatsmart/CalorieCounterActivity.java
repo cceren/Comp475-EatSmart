8b425064ee9d90eb05d28f1bf295d84cd6a788b6
Ken and Isacar Racine <irodrigu@harding.edu>
2014-03-19 16:14:19 -0500

    Added a comment

--- EatSmart/src/edu/harding/android/eatsmart/CalorieCounterActivity.java
+++ EatSmart/src/edu/harding/android/eatsmart/CalorieCounterActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class CalorieCounterActivity extends FragmentActivity {
-
+/*Added another comment*/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
