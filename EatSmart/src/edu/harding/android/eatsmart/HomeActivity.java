e575cd124629cda5a9f95d225c5c47de12bd9850
Ken <ygan1@harding.edu>
2014-04-23 15:00:25 -0500

    release up button

--- EatSmart/src/edu/harding/android/eatsmart/HomeActivity.java
+++ EatSmart/src/edu/harding/android/eatsmart/HomeActivity.java
@@ -4,13 +4,33 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.ImageButton;
 
 public class HomeActivity extends SingleFragmentActivity {
 	private final String USERINFO = "userInfo"; 
+	
+	   @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+	        setContentView(R.layout.activity_fragment);
+			getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebtn);
+	        super.onCreate(savedInstanceState);
+	        ImageButton back = (ImageButton)findViewById(R.id.topButton); 
+	        back.setOnClickListener(new OnClickListener() {  
+	            public void onClick(View v) {   
+	            	onBackPressed();
+	            }  
+	        }); 
+	    }
+	
 	@Override
 	protected Fragment createFragment() {
 		
